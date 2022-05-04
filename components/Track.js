65b32e5c354e3f291d9a1c461df7489e9d387a74
Toby <toby.gates@qubit.com>
2022-05-04 17:06:31 +0100

    Fix default class

--- components/Track.js
+++ components/Track.js
@@ -6,18 +6,24 @@ module.exports = function Track ({ glideClass, children }) {
       <ul className={`${glideClass}__slides`}>
         {children.length
           ? children.map((slide, index) => {
-              if(slide && typeof slide === 'object') {
-                if(Boolean(slide.props)) {
+              if (slide && typeof slide === 'object') {
+                if (Boolean(slide.props)) {
+                  const defaultClass = slide.props.className
+                    ? slide.props.className
+                    : ''
                   return React.cloneElement(slide, {
                     ...slide.props,
                     key: slide.props.key || index,
-                    className: `${glideClass}__slide ${slide.props.className &&
-                      slide.props.className}`
+                    className: `${glideClass}__slide ${defaultClass}`
                   })
                 }
                 return ''
               }
-              return <li key={index} className={`${glideClass}__slide`}>{slide}</li>
+              return (
+                <li key={index} className={`${glideClass}__slide`}>
+                  {slide}
+                </li>
+              )
             })
           : ''}
       </ul>
